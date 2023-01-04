20d6f2a0c9d022e7232e118b7c372c01fbf91604
Isaac Marroquí Penalva <mmarroqui@alu.ucam.edu>
2023-01-04 19:08:56 +0100

    Initial commit

---
+++ jumis/src/test/java/alu/ucam/edu/jumis/AppTest.java
@@ -0,0 +1,20 @@
+package alu.ucam.edu.jumis;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
